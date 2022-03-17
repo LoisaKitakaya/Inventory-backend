dffe991c073334a370057de241bf701099c6f219
Freedom Loisa <kitakayaloisa@gmail.com>
2022-03-17 22:19:45 +0300

    added user authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout as django_logout, login as django_login
+from django.contrib import messages
 
 # Create your views here.
+
+def login(request):
+
+    if request.method == 'POST':
+
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+
+            user = form.get_user()
+
+            django_login(request, user)
+
+            messages.success(request, 'Successfully logged in.')
+
+            return redirect('home')
+
+    else:
+
+        form = AuthenticationForm()
+
+    page_title = 'Login to your account'
+
+    context = {
+        'form' : form,
+        'page_title': page_title,
+    }
+
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+
+    django_logout(request)
+
+    messages.error(request, 'You have been logged out.')
+
+    return redirect('home')
--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import InventoryItem
 from .forms import AddItem
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+
 
 # Create your views here.
 
@@ -10,6 +13,7 @@ def home(request):
 
     return render(request, 'app/index.html', context)
 
+@login_required(login_url='login')
 def store(request):
 
     store_items = InventoryItem.objects.all()
@@ -23,6 +27,7 @@ def store(request):
 
     return render(request, 'app/store.html', context)
 
+@login_required(login_url='login')
 def add_item(request):
 
     if request.method == 'POST':
@@ -48,6 +53,7 @@ def add_item(request):
 
     return render(request, 'app/add_item.html', context)
 
+@login_required(login_url='login')
 def edit_item(request, slug):
 
     item_to_edit = InventoryItem.objects.get(slug=slug)
@@ -75,6 +81,7 @@ def edit_item(request, slug):
 
     return render(request, 'app/edit_item.html', context)
 
+@login_required(login_url='login')
 def delete_item(request, id):
 
     item_to_delete = InventoryItem.objects.get(id=id)
@@ -83,6 +90,7 @@ def delete_item(request, id):
 
     return redirect('store')
 
+@login_required(login_url='login')
 def store_item(request, slug):
 
     item = InventoryItem.objects.filter(slug=slug)
